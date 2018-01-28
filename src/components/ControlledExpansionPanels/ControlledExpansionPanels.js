df36e52fae38e16d81d37d44e749ed7fb1a43c5d
Harish Mohan Raj <hraj@sapient.com>
2018-01-28 19:34:47 +0000

    results component style added

--- src/components/ControlledExpansionPanels/ControlledExpansionPanels.js
+++ src/components/ControlledExpansionPanels/ControlledExpansionPanels.js
@@ -37,6 +37,26 @@ class ControlledExpansionPanels extends React.Component {
     });
   };
 
+  renderResults(classes, expanded) {
+    let arr = []
+    for(let i = 1; i<=30; i++) {
+      arr.push(
+        <ExpansionPanel expanded={expanded === 'panel'+i} onChange={this.handleChange('panel'+i)}>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography className={classes.heading}>Email {i}</Typography>
+              <Typography className={classes.secondaryHeading}>Class B</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography align='center'>
+                The reason goes here...
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+      )
+    }
+    return arr;
+  }
+
   render() {
     const { classes } = this.props;
     const { expanded } = this.state;
@@ -44,46 +64,7 @@ class ControlledExpansionPanels extends React.Component {
     return (
       <div className='results-component'>
         <MuiThemeProvider className={classes.root}>
-          <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className={classes.heading}>General settings</Typography>
-              <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>
-                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
-                maximus est, id dignissim quam.
-              </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel expanded={expanded === 'panel2'} onChange={this.handleChange('panel2')}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className={classes.heading}>Users</Typography>
-              <Typography className={classes.secondaryHeading}>
-                You are currently not an owner
-              </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>
-                Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
-                diam eros in elit. Pellentesque convallis laoreet laoreet.
-              </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel expanded={expanded === 'panel3'} onChange={this.handleChange('panel3')}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className={classes.heading}>Advanced settings</Typography>
-              <Typography className={classes.secondaryHeading}>
-                Filtering has been entirely disabled for whole web server
-              </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
-                eros, vitae egestas augue. Duis vel est augue.
-              </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
+          {this.renderResults(classes, expanded)}
         </MuiThemeProvider>
       </div>
     );
