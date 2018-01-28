c231ad4de7b601ff66749dc241bc2f6c12f569b0
Harish Mohan Raj <hraj@sapient.com>
2018-01-28 20:25:59 +0000

    header removed

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">UI/UX for LIME and surrogate model</h1>
         </header>
         <p className="App-intro">
           <FileUpload />
